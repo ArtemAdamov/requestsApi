9ee0965a71a94a633afd4498cd29dc4c16f1e5d8
Artem Adamov <adamov.ar.v@gmail.com>
2023-05-09 15:10:31 +0600

    added basic upload file endpoint

--- src/Controller/ApplicationController.php
+++ src/Controller/ApplicationController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Application;
 use App\Entity\Client;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\File;
 use App\Repository\ApplicationRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,6 +24,26 @@ class ApplicationController extends AbstractController
         return $this->getUser()->getClient();
     }
 
+    #[Route('/api/applications/attachment', name: 'upload_attachment', methods: 'POST')]
+    public function upload(Request $request,  SerializerInterface $serializer): JsonResponse
+    {
+        $uploadedFile = $request->files->get('attachment');
+        if (!$uploadedFile) {
+            return new JsonResponse(['error' => 'No file provided'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $uploadDirectory = $this->getParameter('kernel.project_dir') . '/public/uploads';
+        $filename = uniqid() . '.' . $uploadedFile->getClientOriginalExtension();
+
+        try {
+            $uploadedFile->move($uploadDirectory, $filename);
+        } catch (FileException $e) {
+            return new JsonResponse(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return new JsonResponse(['fileUrl' => '/uploads/' . $filename], Response::HTTP_CREATED);
+
+    }
     #[Route('/api/applications', name: 'create_application', methods: 'POST')]
     public function create(Request $request, SerializerInterface $serializer, ValidatorInterface $validator, EntityManagerInterface $entityManager): JsonResponse
     {
@@ -52,24 +74,28 @@ class ApplicationController extends AbstractController
     #[Route('/api/applications', name: 'list_applications', methods: 'GET')]
     public function list(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
     {
-        $client = $this->getClient();
         $page = max(1, $request->query->getInt('page', 1));
         $limit = max(1, $request->query->getInt('limit', 10));
         $offset = ($page - 1) * $limit;
+        $user = $this->getUser();
 
-        $query = $entityManager->createQuery(
-            'SELECT a
-            FROM App\Entity\Application a
-            WHERE a.creator = :client
-            ORDER BY a.created_at ASC'
-        )->setParameter('client', $client)
+        $queryBuilder = $entityManager->createQueryBuilder();
+        $queryBuilder->select('a')
+            ->from(Application::class, 'a')
+            ->orderBy('a.created_at', 'ASC')
             ->setFirstResult($offset)
             ->setMaxResults($limit);
 
+        if (!in_array('ROLE_ADMIN', $user->getRoles())) {
+            $client = $this->getClient();
+            $queryBuilder->where('a.creator = :client')
+                ->setParameter('client', $client);
+        }
+
         try {
-            $result = $query->getResult();
+            $result = $queryBuilder->getQuery()->getResult();
         } catch (\Throwable $e) {
-            return new JsonResponse(['error' => $e->getMessage()], Response::HTTP_CONFLICT);
+            return new JsonResponse(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
 
         $responseContent = $serializer->serialize($result, 'json', ['groups' => 'application']);
